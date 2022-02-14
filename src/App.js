6a3d9ebb3e1338cd47a8c523a4b4604899526ff7
im-in123 <rudyelisha77@gmail.com>
2022-02-14 02:12:27 -0800

    blm

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
         href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
         rel="stylesheet"
       />
-      {/* <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500|Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
-                  ></link> */}
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500|Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
+      ></link>
       {/* <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/icon?family=Material+Icons"
